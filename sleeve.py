34b211cd50f4eaf66bd420d716c3ef517540bef0
matmatomate <40830085+matmatomate@users.noreply.github.com>
2025-05-22 18:34:55 +0200

    Add files via upload

---
+++ sleeve.py
@@ -0,0 +1,15 @@
+from swatch import Swatch
+
+class Sleeve:
+
+    def __init__(self, swatch, desired_width, desired_length)
+        self.swatch = swatch
+        self.desired_width = desired_width
+        self.desired_length = desired_length
+        current_width = 0
+        current_length = 0
+        desired_stitches = 0
+        current_stitches = 0
+        desired_rows = 0
+        current_rows = 0
+

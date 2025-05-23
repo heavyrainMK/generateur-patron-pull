e112ac98e8f340925203b1827b9ab6e428ccdffd
matmatomate <40830085+matmatomate@users.noreply.github.com>
2025-05-23 14:25:02 +0200

    Update swatch.py

--- swatch.py
+++ swatch.py
@@ -2,12 +2,20 @@ class Swatch:
     def __init__(self, stitches, rows):
         self.stitches = stitches
         self.rows = rows
+        self.stitchesPerCM = self.stitches / 10
+        self.rowsPerCM = self.rows / 10
 
     def getStitches(self):
         return self.stitches
 
     def getRows(self):
         return self.rows
+
+    def getStitchesPerCM(self):
+        return self.stitchesPerCM
+
+    def getRowsPerCM(self):
+        return self.rowsPerCM
 
     def __str__(self):
         return f"the swatch is {self.stitches} stitches and {self.rows} rows for a 10cm square"

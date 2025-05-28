e93fdf8e9065c45e66b694059a8192b41becc9f3
matmatomate <40830085+matmatomate@users.noreply.github.com>
2025-05-28 16:24:49 +0200

    Add files via upload

---
+++ sweater.py
@@ -0,0 +1,10 @@
+class Sweater:
+
+    def __init__(self, right_sleeve, left_sleeve, front, back, swatch):
+        self.right_sleeve = right_sleeve
+        self.left_sleeve = left_sleeve
+        self.front = front
+        self.back = back
+        self.swatch = swatch
+
+    

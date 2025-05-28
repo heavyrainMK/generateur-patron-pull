f23eecae7240c1990c0617c5bdbd06ce1e8876f2
matmatomate <40830085+matmatomate@users.noreply.github.com>
2025-05-28 16:20:47 +0200

    move calculs to sweater class.py

--- back.py
+++ back.py
@@ -1,6 +1,6 @@
 class Back:
 
-    def __init__(self, chest_width, armhole_depth, underarm_to_hem_length): 
+    def __init__(self, chest_width, armhole_depth, underarm_to_hem_length) 
         self.chest_width = chest_width
         self.armhole_depth = armhole_depth
         self.underarm_to_hem_length = underarm_to_hem_length
@@ -17,23 +17,23 @@ class Back:
     def getUnderArmToHemLength(self):
         return self.underarm_to_hem_length
 
-    def setChestStitches(self, stitches_per_cm):
-        self.chest_stitches = self.chest_width * stitches_per_cm
+    def setChestStitches(self, nb_of_stitches):
+        self.chest_stitches = nb_of_stitches
 
     def getChestStitches(self):
         return self.chest_stitches
 
-    def setRowsToUnderarm(self, rows_per_cm):
-        self.rows_to_underarm = self.armhole_depth * rows_per_cm
+    def setRowsToUnderarm(self, nb_of_rows):
+        self.rows_to_underarm = nb_of_rows
 
     def getRowsToUnderarm(self):
         return self.rows_to_underarm
 
-    def setRowsToHem(self, rows_per_cm):
-        self.rows_to_hem = self.underarm_to_hem_length * rows_per_cm
+    def setRowsToHem(self, nb_of_rows):
+        self.rows_to_hem = nb_of_rows
 
     def getRowsToHem(self):
         return self.rows_to_hem
 
     def __str__(self):
-        return f"Le dos mesure {self.armhole_depth}CM depuis l'encolure jusqu'a l'aisselle et {self.underarm_to_hem_length}CM de l'aisselle jusqu'a l'ourlet. Le tour de poitrine est {self.chest_width}"
+        return f"Le dos mesure {self.armhole_depth}CM depuis l'encolure jusqu'a l'aisselle et {self.underarm_to_hem_length}CM de l'aisselle jusqu'a l'ourlet. Le tour de poitrine est {self.chest_width}

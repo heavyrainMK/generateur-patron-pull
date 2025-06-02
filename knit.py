9ae4c11a2548de0fbd75e226355bc67b18a24c51
mathilde <mathilde@laptop.com>
2025-06-02 16:18:04 +0200

    doing some calculations

--- knit.py
+++ knit.py
@@ -5,16 +5,16 @@ from swatch import Swatch
 import math
 
 #don't forget to round up the numbers
-def calculStitchesNeeded(self, width):
-        return math.ceil(self.swatch.getStitches() / 10 * width)
+def calculStitchesNeeded(stitches, width):
+        return math.ceil(stitches / 10 * width)
 
-def calculRowsNeeded(self, length):
-        return math.ceil(self.swatch.getRows() / 10 * length)
+def calculRowsNeeded(rows, length):
+        return math.ceil(rows) / 10 * length)
 
-def calculIncreases(self, width_1, width_2):
+def calculIncreases(width_1, width_2):
         return width_2 - width_1
 
-def calculDecreases(self, width_1, width_2):
+def calculDecreases(width_1, width_2):
         return width_1 - width_2
 
 
@@ -25,7 +25,10 @@ def main():
     my_front = Front()
     my_back = Back()
     my_swatch = Swatch()
-    
+
+
+    my_back.setNeckStitches(calculStitchesNeeded(my_swatch.getStitches(), my_back.getNeckWidth()))
+    my_right_sleeve.setTopSleeveStitches(calculStitchesNeeded(my_swatch.getStitches(), my_right_sleeve.getTopSleeveWidth()))
 
 
 

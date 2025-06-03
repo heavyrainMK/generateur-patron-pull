765588b1043e37012594c81114cfdc226429a459
mathilde <mathilde@laptop.com>
2025-06-03 07:13:27 +0200

    debut des calculs dans knit.py

--- knit.py
+++ knit.py
@@ -9,7 +9,7 @@ def calculStitchesNeeded(stitches, width):
         return math.ceil(stitches / 10 * width)
 
 def calculRowsNeeded(rows, length):
-        return math.ceil(rows) / 10 * length)
+        return math.ceil(rows / 10 * length)
 
 def calculIncreases(width_1, width_2):
         return width_2 - width_1
@@ -20,19 +20,21 @@ def calculDecreases(width_1, width_2):
 
 def main():
     #il faut construire les objets avec les mesures fournis par l'utilisateur
-    my_right_sleeve = Sleeve()
-    my_left_sleeve = Sleeve()
-    my_front = Front()
-    my_back = Back()
-    my_swatch = Swatch()
+    my_front = Front(tour_de_poitrine, longueur_aisselle, longueur_bas)
+    my_back = Back(largeur_encolure, tour_de_poitrine, longueur_aisselle, longueur_bas)
+    my_right_sleeve = Sleeve(tour_de_bras, tour_de_poignet, longueur_aisselle, longueur_poignet)
+    my_left_sleeve = Sleeve(tour_de_bras, tour_de_poignet, longueur_aisselle, longueur_poignet)
 
+    my_swatch = Swatch(mailles, rangs)
 
+    #calcul des mailles au montage
     my_back.setNeckStitches(calculStitchesNeeded(my_swatch.getStitches(), my_back.getNeckWidth()))
     my_right_sleeve.setTopSleeveStitches(calculStitchesNeeded(my_swatch.getStitches(), my_right_sleeve.getTopSleeveWidth()))
+    my_left_sleeve.setTopSleeveStitches(calculStitchesNeeded(my_swatch.getStitches(), my_left_sleeve.getTopSleeveWidth()))
 
 
 
     if __name__ == "__main__":
-    main()
+        main()
 
     

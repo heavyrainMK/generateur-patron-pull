fba79b58d9d2c2f6935a68755b0a819e3f89c033
mathilde <mathilde@laptop.com>
2025-06-10 15:33:52 +0200

    calculs pour le corps

--- knit.py
+++ knit.py
@@ -31,7 +31,7 @@ def main():
     my_right_sleeve = Sleeve(28, 18.5, 25, 45)
     my_left_sleeve = Sleeve(28, 18.5, 25, 45)
 
-    my_swatch = Swatch(28, 32)
+    my_swatch = Swatch(25, 39)
     aisance_corps = 5
     aisance_manches = 2
 
@@ -66,17 +66,63 @@ def main():
     #il faut donc bien penser a les soustraire au nombre d'augmentation pour ne pas se retrouver avec des manches/un corps trop large
     #je compte une largeur d'aisselle de 3cm, changerai peut etre plus tard
     nb_de_mailles_aisselle = calculStitchesNeeded(my_swatch.getStitches(), 3, 0)
+    print('mailles aisselle : ' + str(nb_de_mailles_aisselle))
+
+    print('apres separation : \ndevant : ' + str(my_front.getChestStitches()) + '\nmanche droite : ' + str(my_right_sleeve.getUpperarmStitches()) + '\ndos : ' + str(my_back.getChestStitches()) + '\nmanche gauche : ' + str(my_left_sleeve.getUpperarmStitches()) + '\nraglan : 4')
 
     #on fait le meme nombre d'augmentations devant et derriere, donc on ne fait qu'une fois le calcul
     #les augmentation/diminutions vont toujours par paire, une au debut une a la fin
     #on divise donc par 2 pour savoir le nombre de rangs qui contiendront des augmentations
-    nb_augmentations_dos = (calculIncreases(my_back.getChestStitches(), my_back.getNeckStitches()) - nb_de_mailles_aisselle) / 2
+    #il ne faut pas oublier de soustraire les mailles de l'aisselle pour le devant, le dos et les manches, ainsi que les mailles raglan (pour le devant et le dos seulement)
+    nb_augmentations_dos = math.ceil((calculIncreases(my_back.getNeckStitches(), my_back.getChestStitches()) - nb_de_mailles_aisselle - 4) / 2)
     #les deux manches sont pareilles, on ne fait qu'une fois le calcul
-    nb_augmentations_manches = (calculIncreases(my_left_sleeve.getTopSleeveStitches(), my_left_sleeve.getUpperarmStitches()) - nb_de_mailles_aisselle) / 2
+    nb_augmentations_manches = math.ceil((calculIncreases(my_left_sleeve.getTopSleeveStitches(), my_left_sleeve.getUpperarmStitches()) - nb_de_mailles_aisselle)  / 2)
+
+    print('augmentations pour le corps : ' + str(nb_augmentations_dos))
+    print('augmentations pour les manches : ' + str(nb_augmentations_manches))
 
     #calcul du nombre de rangs pour arriver jusqu'a l'emmanchure
     #la profondeur d'emmanchure est la meeme pour le devant, le dos et les manches, on fait le calcul une seule fois
     my_back.setRowsToUnderarm(calculRowsNeeded(my_swatch.getRows(), my_back.getArmholeDepth()))
+    print(str(my_back.getRowsToUnderarm()) + ' rangs avant aisselle')
+
+    #il existe plusieurs schemas d'augmentation en fonction des morphologie
+    #on peut combiner des augmentations tous les rangs avec des augmentations tous les 2 rangs
+    #ou bien des augmentations tous les 2 puis tous les 4 rangs
+    #on peut egalement combiner des augmentations sur le corps et les manches en meme temps, ou bien seulement les manche, ou seulement le corps
+
+    # corps_augmentation_tous_les_rangs
+    # corps_augmentation_tous_les_2_rangs
+    # corps_augmentation_tous_les_4_rangs
+    # manche_augmentation_tous_les_rangs
+    # manche_augmentation_tous_les_2_rangs
+    # manche_augmentation_tous_les_4_rangs
+    if ((my_back.getRowsToUnderarm() / 2) > nb_augmentations_dos or (my_back.getRowsToUnderarm() / 2) > nb_augmentations_manches):
+    
+        corps_augmentation_tous_les_4_rangs = math.floor(my_back.getRowsToUnderarm() / 4)
+        corps_augmentation_tous_les_2_rangs = nb_augmentations_dos - corps_augmentation_tous_les_4_rangs
+        corps_augmentation_tous_les_rangs = 0
+        manche_augmentation_tous_les_4_rangs = math.floor(my_back.getRowsToUnderarm() / 4)
+        manche_augmentation_tous_les_2_rangs = nb_augmentations_manches - manche_augmentation_tous_les_4_rangs
+        manche_augmentation_tous_les_rangs = 0
+
+    else:
+        corps_augmentation_tous_les_4_rangs = 0
+        corps_augmentation_tous_les_2_rangs = math.floor(my_back.getRowsToUnderarm() / 2)
+        corps_augmentation_tous_les_rangs = nb_augmentations_dos - corps_augmentation_tous_les_2_rangs
+        manche_augmentation_tous_les_4_rangs = 0
+        manche_augmentation_tous_les_2_rangs = math.floor(my_back.getRowsToUnderarm() / 2)
+        manche_augmentation_tous_les_rangs = nb_augmentations_manches - manche_augmentation_tous_les_2_rangs
+    
+        
+
+    
+    print('corps : ' + str(corps_augmentation_tous_les_4_rangs) + ' augmentations tous les 4 rangs, ' + str(corps_augmentation_tous_les_2_rangs) + ' augmentations tous les 2 rangs et ' + str(corps_augmentation_tous_les_rangs) + ' augmentations tous les rangs\n')
+
+
+    print('manches : ' + str(manche_augmentation_tous_les_4_rangs) + ' augmentations tous les 4 rangs, ' + str(manche_augmentation_tous_les_2_rangs) + ' augmentations tous les 2 rangs et ' + str(manche_augmentation_tous_les_rangs) + ' augmentations tous les rangs\n')
+
+
 
 if __name__ == "__main__":
     main()

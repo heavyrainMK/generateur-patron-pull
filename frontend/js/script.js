d354d30c60a3f54663eb09323de709fdec70a373
heavyrainMK <maxim.khomenko18@gmail.com>
2025-07-24 14:34:13 +0700

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -400,7 +400,7 @@ async function genererPatron() {
             delete donnees['aisance_manches'];
         }
 
-        const reponse = await fetch('api/calculer-patron', {
+        const reponse = await fetch('/api/calculer-patron', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
